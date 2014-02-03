636c9e670de6efaafffc6abafc396de93a193787
Alex Major <alex@hobzy.com>
2014-02-03 17:22:01 +0000

    Bugfix missing ModuleOptions service manager name

--- config/module.config.php
+++ config/module.config.php
@@ -3,6 +3,7 @@
 return array(
     'service_manager' => array(
         'factories' => array(
+            'Twilio\Options\ModuleOptions' => 'Twilio\Options\Factory\ModuleOptionsFactory',
             'Twilio\Service\TwilioService' => 'Twilio\Service\TwilioServiceFactory'
         )
     )
